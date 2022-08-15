338028fefd269e46ba247b227a9d3981eedf9123
Emre Mert <emre.mrt98@gmail.com>
2022-08-15 19:09:55 +0300

    second commit

--- src/pages/analytics.jsx
+++ src/pages/analytics.jsx
@@ -1,49 +1,148 @@
 import React from 'react';
 import "./analytics/analytic.css"
 import Analytic from './analytics/analytic'
+import { BsFillCircleFill } from "react-icons/bs";
 
 
-const analyticTable = [
-    {
-        id: 1,
-        value: 1460,
-        maxValue: 3758,
-        name: "Word Count"
-    },
-    {
-        id: 2,
-        value: 6,
-        maxValue: 24,
-        name: "Spending Time"
-    },
-    {
-        id: 3,
-        value: 75,
-        maxValue: 100,
-        name: "Arrive Target"
-    },
-    {
-        id: 4,
-        value: 3,
-        maxValue: 24,
-        name: "Spending Time for Game"
-    }
-]
 
 function analytics() {
-
+    
+    const analyticTable = [
+        {
+            id: 1,
+            value: 1460,
+            maxValue: 3758,
+            name: "Word Count",
+            color: "conic-gradient(#1C4FB2 20deg, #1c4eb27e 0deg)"
+        },
+        {
+            id: 2,
+            value: 6,
+            maxValue: 24,
+            name: "Spending Time",
+            color: "conic-gradient(#D90A0A 100deg, #d90a0a7a 0deg)"
+        },
+        {
+            id: 3,
+            value: 75,
+            maxValue: 100,
+            name: "Arrive Target",
+            color: "conic-gradient(#50A663 270deg, #50a6637a 0deg)"
+        },
+        {
+            id: 4,
+            value: 3,
+            maxValue: 24,
+            name: "Spending Time for Game",
+            color: "conic-gradient(#CD9E42 50deg, #cd9f4288 0deg)"
+        }
+    ]
+    
+    const studentList = [
+        {
+            name: "Emre Mert",
+            isActive: false
+        },
+    
+        {
+            name: "Emre Mert",
+            isActive: false
+        },
+    
+        {
+            name: "Emre Mert",
+            isActive: true
+        },
+    
+        {
+            name: "Emre Mert",
+            isActive: false
+        },
+    
+        {
+            name: "Emre Mert",
+            isActive: true
+        },
+    
+        {
+            name: "Emre Mert",
+            isActive: false
+        },
+    
+        {
+            name: "Emre Mert",
+            isActive: true
+        },
+    
+        {
+            name: "Emre Mert",
+            isActive: true
+        },
+    
+        {
+            name: "Emre Mert",
+            isActive: true
+        },
+    
+        {
+            name: "Emre Mert",
+            isActive: true
+        },
+    
+        {
+            name: "Emre Mert",
+            isActive: false
+        },
+        {
+            name: "Emre Mert",
+            isActive: true
+        },
+    
+        {
+            name: "Emre Mert",
+            isActive: true
+        },
+    
+        {
+            name: "Emre Mert",
+            isActive: false
+        },
+        {
+            name: "Emre Mert",
+            isActive: false
+        },
+        {
+            name: "Emre Mert",
+            isActive: false
+        }
+    
+    ]
     return (
         <div>
             <div style={{ borderBottom: '1px solid black' }}>
                 <h1>Analytics</h1>
             </div>
             <div className='main-contain'>
-            <Analytic data = {analyticTable[0]} rank = "conic-gradient(#7d2ae8 200deg, #ededed 0deg)"/>
-            <Analytic data = {analyticTable[1]} rank = "conic-gradient(#7d2ae8 92deg, #ededed 0deg)"/>
-            <Analytic data = {analyticTable[2]} rank = "conic-gradient(#7d2ae8 268deg, #ededed 0deg)"/>
-            <Analytic data = {analyticTable[3]} rank = "conic-gradient(#7d2ae8 30deg, #ededed 0deg)"/>
+                <div className='contain'>
+                    <Analytic data={analyticTable[0]} rank={analyticTable[0].color} />
+                    <Analytic data={analyticTable[1]} rank={analyticTable[1].color} />
+                    <Analytic data={analyticTable[2]} rank={analyticTable[2].color} />
+                    <Analytic data={analyticTable[3]} rank={analyticTable[3].color} />
+                </div>
+
+                <div>
+                    <div className="head">
+                        <h2 className='header'>Student List</h2>
+                        <p className='userCount'>09 / 16</p>
+                    </div>
+                    <div className="student">
+                        {
+                            studentList.map((person, index) => (<p key={index} className='information' ><span className='person'>{person.name}</span> <span><BsFillCircleFill className='activeUser'  style={person.isActive ? { "color": "green" } : { "color": "red" }} /></span></p>))
+                        }
+                    </div>
+                </div>
             </div>
-        </div>
+        </div >
 
     );
 };
--- src/pages/analytics/analytic.jsx
+++ src/pages/analytics/analytic.jsx
@@ -1,16 +1,14 @@
 import { useState } from "react";
 
 export default function Analytic({ data, rank }) {
-    console.log(rank);
-    //const [colorValue, setColorValue] = useState(rank)
-    //console.log(colorValue);
+    const [color, setColor] = useState(0);
     return (
 
         <div className="general-analytic">
 
             <div className="general-container" >
                 <div className='inner' >
-                    <div className="circular-progress" style={{"background": rank}}>
+                    <div className="circular-progress" style={{ "background": rank }}>
                         <span className="progress-value">{`${data.value} / ${data.maxValue}`}</span>
                     </div>
                     <span className="text">{data.name}</span>
